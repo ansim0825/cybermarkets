38ae42fceb782430d14e28cabeb48b7fdc72d962
ansim0825 <andysimko@gmail.com>
2026-01-13 19:05:46 -0600

    updates to email-security cloudflare and dynamic routing - 4

--- app/pillars/network-edge/email-security/companies/[company]/page.jsx
+++ app/pillars/network-edge/email-security/companies/[company]/page.jsx
@@ -1,48 +1,8 @@
-import { notFound } from 'next/navigation';
-
-// Import all company pages directly
-import AbnormalPage from '../abnormal';
-import BarracudaPage from '../barracuda';
-import CheckpointPage from '../checkpoint';
-import CiscoPage from '../cisco';
-import CloudflarePage from '../cloudflare';
-import DarktracePage from '../darktrace';
-import FortinetPage from '../fortinet';
-import IronscalesPage from '../ironscales';
-import Knowbe4Page from '../knowbe4';
-import MaterialsecurityPage from '../materialsecurity';
-import MicrosoftPage from '../microsoft';
-import MimecastPage from '../mimecast';
-import ProofpointPage from '../proofpoint';
-import SophosPage from '../sophos';
-import TrendmicroPage from '../trendmicro';
-
-const companyPages = {
-  abnormal: AbnormalPage,
-  barracuda: BarracudaPage,
-  checkpoint: CheckpointPage,
-  cisco: CiscoPage,
-  cloudflare: CloudflarePage,
-  darktrace: DarktracePage,
-  fortinet: FortinetPage,
-  ironscales: IronscalesPage,
-  knowbe4: Knowbe4Page,
-  materialsecurity: MaterialsecurityPage,
-  microsoft: MicrosoftPage,
-  mimecast: MimecastPage,
-  proofpoint: ProofpointPage,
-  sophos: SophosPage,
-  trendmicro: TrendmicroPage,
-};
-
 export default function CompanyPage({ params }) {
-  const company = params?.company?.toLowerCase();
-  
-  const PageComponent = companyPages[company];
-  
-  if (!PageComponent) {
-    notFound();
+    return (
+      <div style={{ padding: '50px', color: 'white', background: '#111' }}>
+        <h1>Company: {params?.company}</h1>
+        <p>If you see this, routing works!</p>
+      </div>
+    );
   }
-  
-  return <PageComponent />;
-}
