2fada921fa96ec32bd3cf617012516d6b7601296
ansim0825 <andysimko@gmail.com>
2026-01-13 19:23:12 -0600

    troubleshooting routing-4

--- app/pillars/network-edge/email-security/companies/[company]/page.jsx
+++ app/pillars/network-edge/email-security/companies/[company]/page.jsx
@@ -1,20 +1,48 @@
 import { notFound } from 'next/navigation';
+import { use } from 'react';
 
-// Only import Cloudflare to test
+// Import all company pages directly
+import AbnormalPage from '../abnormal';
+import BarracudaPage from '../barracuda';
+import CheckpointPage from '../checkpoint';
+import CiscoPage from '../cisco';
 import CloudflarePage from '../cloudflare';
+import DarktracePage from '../darktrace';
+import FortinetPage from '../fortinet';
+import IronscalesPage from '../ironscales';
+import Knowbe4Page from '../knowbe4';
+import MaterialsecurityPage from '../materialsecurity';
+import MicrosoftPage from '../microsoft';
+import MimecastPage from '../mimecast';
+import ProofpointPage from '../proofpoint';
+import SophosPage from '../sophos';
+import TrendmicroPage from '../trendmicro';
 
 const companyPages = {
+  abnormal: AbnormalPage,
+  barracuda: BarracudaPage,
+  checkpoint: CheckpointPage,
+  cisco: CiscoPage,
   cloudflare: CloudflarePage,
+  darktrace: DarktracePage,
+  fortinet: FortinetPage,
+  ironscales: IronscalesPage,
+  knowbe4: Knowbe4Page,
+  materialsecurity: MaterialsecurityPage,
+  microsoft: MicrosoftPage,
+  mimecast: MimecastPage,
+  proofpoint: ProofpointPage,
+  sophos: SophosPage,
+  trendmicro: TrendmicroPage,
 };
 
 export default function CompanyPage({ params }) {
-  const company = params?.company?.toLowerCase();
+  const { company } = use(params);
   
-  const PageComponent = companyPages[company];
+  const PageComponent = companyPages[company?.toLowerCase()];
   
   if (!PageComponent) {
     notFound();
   }
   
   return <PageComponent />;
-}
